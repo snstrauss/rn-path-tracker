11c52c0f50b3444f383e5dcc5cf94adc46b8894e
snstrauss <zahavi.guy.85@gmail.com>
2020-10-10 11:43:35 +0300

    added 'logout' button to account screen

--- client/screens/AccountScreen.js
+++ client/screens/AccountScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import React, { useContext } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-navigation';
+import { AuthContext } from '../context/AuthProvider';
+import { logout } from '../services/login.service';
 
 const S = StyleSheet.create({
-
+    button: {
+        color: 'white',
+        backgroundColor: 'firebrick',
+        fontSize: 30,
+        textTransform: 'uppercase',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        margin: 50,
+        padding: 17,
+    },
+    title: {
+        fontSize: 50
+    }
 })
 
-export default function AccountScreen(){
+export default function AccountScreen({ navigation: { navigate } }){
+
+    const {methods: authMethods} = useContext(AuthContext);
+
+    function doLogout(){
+        logout();
+        authMethods.signout();
+        navigate('signin');
+    }
+
     return (
-        <Text>account</Text>
+        <SafeAreaView forceInset={{top: 'always'}}>
+            <Text style={S.title}>ACCOUNT</Text>
+            <TouchableOpacity onPress={doLogout}>
+                <Text style={S.button}>
+                    Log Out
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     )
 }
